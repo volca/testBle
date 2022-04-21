a145ba538eb34bf757caeb1c90fef9b22310a1dc
yanc <volcas@gmail.com>
2022-04-21 10:11:53 +0800

    Initial Commit

---
+++ testBle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testBle
+//
+//  Created by yy on 2022/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testBle/testBleApp.swift
@@ -0,0 +1,17 @@
+//
+//  testBleApp.swift
+//  testBle
+//
+//  Created by yy on 2022/4/21.
+//
+
+import SwiftUI
+
+@main
+struct testBleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
